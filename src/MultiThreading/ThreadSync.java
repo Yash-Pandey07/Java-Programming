bd1866d5b5dafd7b6d26b5b7b9419b05810da963
Yash Pandey <ypandey777@yahoo.in>
2025-01-05 23:15:37 +0530

    synchronized

--- src/MultiThreading/ThreadSync.java
+++ src/MultiThreading/ThreadSync.java
@@ -7,18 +7,19 @@ class Account {
     Account(int inputBalance){
         balance = inputBalance;
     }
-    boolean isSufficientBalance(int withdrawAmount){
-        if (balance > withdrawAmount){
+
+    boolean isSufficientBalance(int withdrawAmount) {
+        if (balance >= withdrawAmount) {
             return true;
         }
         else {
             return false;
         }
     }
-    void withdraw(int amountWithdraw){
+    void withdraw(int amountWithdraw, String g1) {
         balance = balance - amountWithdraw;
-        System.out.println("Transaction Successful ");
-        System.out.println("Current balance is "+ balance);
+        System.out.println(g1 + " Transaction Successful ");
+        System.out.println(g1 + " Current balance is "+ balance);
 
     }
 
@@ -26,30 +27,32 @@ class Account {
 
 class Customer implements Runnable{
     Account accountDetails;
-    String userName ;
-    Customer(Account customerDetails , String Name){
-            accountDetails = customerDetails;
-            userName = Name;
+    String userName;
+
+    Customer(Account customerDetails, String Name) {
+        accountDetails = customerDetails;
+        userName = Name;
     }
 
     public void run(){
         Scanner s1 = new Scanner(System.in);
-        System.out.println("Enter amount to withdraw");
-        int amountEntered = s1.nextInt();
-        if (accountDetails.isSufficientBalance(amountEntered)){
-            accountDetails.withdraw(amountEntered);
-        }else {
-            System.out.println("Insufficient Balance ");
+        synchronized (accountDetails) {
+            System.out.println("Enter amount to withdraw for "+ userName);
+            int amountEntered = s1.nextInt();
+            if (accountDetails.isSufficientBalance(amountEntered)){
+                accountDetails.withdraw(amountEntered , userName);
+            }else {
+                System.out.println("Insufficient Balance for "+ userName);
+            }
         }
-
     }
 }
 
 public class ThreadSync {
     public static void main(String[] args) {
         Account a1 = new Account(5000);
-        Customer c1 = new Customer(a1,"C1");
-        Customer c2 = new Customer(a1, "C2");
+        Customer c1 = new Customer(a1, "User 1");
+        Customer c2 = new Customer(a1, "User 2");
 
         Thread t1 = new Thread(c1);
         Thread t2 = new Thread(c2);
