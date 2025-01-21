e2082deb62eca26613868f437dc56d0caed6d0f3
Yash Pandey <ypandey777@yahoo.in>
2025-01-21 23:14:29 +0530

    NumberIslandsLeetcode

---
+++ src/week4/Temp1.java
@@ -0,0 +1,25 @@
+package week4;
+
+public class Temp1
+{
+    public static void main(String[] args) {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+//                System.out.println(i+" "+j);
+            }
+        }
+
+        // integer-type
+        System.out.println(10 == 20);
+
+        // char-type
+        System.out.println('a' == 'b');
+
+        // char and double type
+        System.out.println('a' == 97.0);
+
+        // boolean type
+        System.out.println(true == true);
+
+    }
+}
