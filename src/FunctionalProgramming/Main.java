d5acd95d6d8b01930ddb7da9267c50eec5eedf32
Yash Pandey <44584614+Yash-Pandey07@users.noreply.github.com>
2025-02-28 23:59:51 +0530

    Update Main.java

--- src/FunctionalProgramming/Main.java
+++ src/FunctionalProgramming/Main.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 import static FunctionalProgramming.Main.Gender.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         List<Person> people = List.of(
                 new Person("John", MALE),
                 new Person("Mickel", MALE),
